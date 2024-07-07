71763e40746165463ca4cb4fbb5cdb91abe73a08
Joynal Alam <joynalam67598@gmail.com>
2024-07-08 00:15:18 +0600

    customize sorting

---
+++ GraphQLDemo.API/GraphQLDemo.API/Schema/Sorters/CourseSortType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQLDemo.API.Schema.Queries;
+using HotChocolate.Data.Sorting;
+
+namespace GraphQLDemo.API.Schema.Sorters
+{
+    public class CourseSortType : SortInputType<CourseType>
+    {
+        protected override void Configure(ISortInputTypeDescriptor<CourseType> descriptor)
+        {
+            descriptor.Ignore(c => c.Id);
+            descriptor.Ignore(c => c.InstructorId);
+
+            // rename the field. added this just for demo.
+            descriptor.Field(c => c.Name).Name("CourseName");
+
+            base.Configure(descriptor);
+        }
+    }
+}
