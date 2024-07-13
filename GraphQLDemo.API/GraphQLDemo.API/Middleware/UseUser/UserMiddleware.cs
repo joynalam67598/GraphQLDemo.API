844e8f2e03bb1dd2e360020eb2b127c780baa230
Joynal Alam <joynalam67598@gmail.com>
2024-07-14 01:54:31 +0600

    extract an user in the field middleware

--- GraphQLDemo.API/GraphQLDemo.API/Middleware/UseUser/UserMiddleware.cs
+++ GraphQLDemo.API/GraphQLDemo.API/Middleware/UseUser/UserMiddleware.cs
@@ -1,4 +1,7 @@
-﻿using HotChocolate.Resolvers;
+﻿using FirebaseAdminAuthentication.DependencyInjection.Models;
+using GraphQLDemo.API.Models;
+using HotChocolate.Resolvers;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace GraphQLDemo.API.Middleware.UseUser
@@ -14,6 +17,20 @@ namespace GraphQLDemo.API.Middleware.UseUser
 
         public async Task Invoke(IMiddlewareContext context)
         {
+            if (context.ContextData.TryGetValue("ClaimsPrincipal", out object rawClaimsPrincipal)
+                && rawClaimsPrincipal is ClaimsPrincipal claimsPrincipal)
+            {
+                bool emailVerified = bool.TryParse(claimsPrincipal.FindFirstValue(FirebaseUserClaimType.EMAIL_VERIFIED),
+                                                    out bool result) ? result: false;
+
+                User user = new User()
+                {
+                    Id = claimsPrincipal.FindFirstValue(FirebaseUserClaimType.ID),
+                    UserName = claimsPrincipal.FindFirstValue(FirebaseUserClaimType.USERNAME),
+                    Email = claimsPrincipal.FindFirstValue(FirebaseUserClaimType.Email),
+                    EmailVerified = emailVerified,
+                }
+            }
             await _next(context);
         }
     }
---
+++ GraphQLDemo.API/GraphQLDemo.API/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphQLDemo.API.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public bool EmailVerified  { get; set; }
+    }
+}
