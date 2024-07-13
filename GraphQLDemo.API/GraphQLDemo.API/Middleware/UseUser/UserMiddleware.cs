62555d9dc942502c7e4bf4e35b3bf248d1026aa3
Joynal Alam <joynalam67598@gmail.com>
2024-07-14 01:30:47 +0600

    create field middleware.

---
+++ GraphQLDemo.API/GraphQLDemo.API/Middleware/UseUser/UserMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using HotChocolate.Resolvers;
+
+namespace GraphQLDemo.API.Middleware.UseUser
+{
+    public class UserMiddleware
+    {
+        private readonly FieldDelegate _next;
+
+        public UserMiddleware(FieldDelegate next)
+        {
+            _next = next;
+        }
+
+        public void Invoike(IMiddlewareContext context)
+        {
+            _next(context);
+        }
+    }
+}
